858a08cf8ce9c68bde1e34f1bbc4ff8f8fe0e204
jaskonius <dev@jaskonius.dev>
2023-12-30 23:29:40 +0100

    added playlists widget

--- echo-tui/src/ui/playlists.rs
+++ echo-tui/src/ui/playlists.rs
@@ -1,5 +1,23 @@
 use crate::app::App;
 use ratatui::layout::Rect;
+use ratatui::prelude::{Modifier, Style};
+use ratatui::widgets::{Block, BorderType, Borders, List, ListItem, ListState};
 use ratatui::Frame;
 
-pub fn render_playlists(_app: &mut App, chunk: Rect, frame: &mut Frame) {}
+pub fn render_playlists(_app: &mut App, chunk: Rect, frame: &mut Frame) {
+    let items = ["Playlist 1", "Playlist 2", "Playlist 3"].map(ListItem::new);
+
+    let list = List::new(items)
+        .highlight_style(Style::default().add_modifier(Modifier::BOLD))
+        .highlight_symbol(">")
+        .block(
+            Block::default()
+                .title("Playlists")
+                .borders(Borders::ALL)
+                .border_type(BorderType::Rounded),
+        );
+
+    let mut state = ListState::default().with_selected(Some(2));
+
+    frame.render_stateful_widget(list, chunk, &mut state);
+}
